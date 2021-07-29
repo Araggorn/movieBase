cb1720602e3363ac5fc691599a1a20e456e6af31
Araggorn <zerelik@gmail.com>
2021-07-29 22:01:00 +0200

    initisl

---
+++ src/main/java/com/example/moviebase/MoviebaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviebaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviebaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moviebase/MoviebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviebaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
